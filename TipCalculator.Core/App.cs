b18ddf0b9463cad90df38c89f416d9bfcb2f9aee
Anderson <andzerotox@gmail.com>
2020-01-19 00:53:58 -0300

    First commit with interfaces

---
+++ TipCalculator.Core/App.cs
@@ -0,0 +1,24 @@
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TipCalculator.Core.ViewModels;
+
+namespace TipCalculator.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            // Agrega la injecion de dependencia de todo lo que termine en services //
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+            // Por donde incializa la applicacion //
+            RegisterAppStart<TipViewModel>();
+        }
+
+    }
+}
---
+++ TipCalculator.Core/ViewModels/TipViewModel.cs
@@ -0,0 +1,66 @@
+﻿using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TipCalculator.Core.Services;
+
+namespace TipCalculator.Core.ViewModels
+{
+    public class TipViewModel : MvxViewModel
+    {
+        private readonly ICalculationService _CalculationService;
+        private decimal _subTotal;
+        private int _generosity;
+        private decimal _tip;
+
+        public TipViewModel(ICalculationService calculationService)
+        {
+            _CalculationService = calculationService;
+        }
+
+        public decimal SubTotal
+        {
+            get => _subTotal;
+            set
+            {
+                _subTotal = value;
+                RaisePropertyChanged(() => SubTotal);
+                Recalculate();
+            }
+        }
+
+        public decimal Tip
+        {
+            get => _tip;
+            set
+            {
+                _tip = value;
+                RaisePropertyChanged(() => Tip);
+            }
+        }
+
+        public int Generosity
+        {
+            get => _generosity;
+            set
+            {
+                _generosity = value;
+                RaisePropertyChanged(() => Generosity);
+                Recalculate();
+            }
+        }
+        public async override Task Initialize()
+        {
+            await base.Initialize();
+            _subTotal = 100;
+            _generosity = 10;
+            Recalculate();
+        }
+
+        private void Recalculate()
+        {
+            Tip = _CalculationService.TipAmount(_subTotal, _generosity);
+        }
+    }
+}
